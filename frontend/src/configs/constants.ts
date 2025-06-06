f5f73bde604540739dc06c480c4c73820a1a3262
Sudarshan kakad <sudarshankakad7672@gmail.com>
2025-06-06 16:15:38 +0530

    backend production url added

--- frontend/src/configs/constants.ts
+++ frontend/src/configs/constants.ts
@@ -1,5 +1,5 @@
 // Development URL
 // const BACKEND_URL: string = "http://localhost:8080/api/v1";
 // Production URL
-const BACKEND_URL: string = "https://ecommerce-backend-1.onrender.com/api/v1";
+const BACKEND_URL: string = "smartmark-backend.gurubrandingservices.com/api/v1";
 export default BACKEND_URL;
